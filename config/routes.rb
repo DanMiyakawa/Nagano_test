e9052bf87dcd58218fba064db13421bc6aec96ad
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-01 23:04:11 +0000

    signup 画面終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
