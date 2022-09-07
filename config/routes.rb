ddba812c6236ba2247f7f34bf23d5d3e5b596206
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-07 05:00:55 +0000

    [admin/order] 表示終了

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
     resources :items, only: [:show, :index, :new, :edit, :create, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    get 'homes/top' => 'homes#top'
+    resources :orders, only: [:show, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
