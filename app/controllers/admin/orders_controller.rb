425111af5cf806de1a4955d5fab3bfd4ddddc77b
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-09-07 09:47:34 +0000

    [order/status] start

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,9 +2,9 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def order_params
     params.require(:order).permit(:shipping_postal_code, :shipping_address, :shipping_name, :payment_method, :total_payment, :postal_code, :address, :name)
   end
-  
+
 end
